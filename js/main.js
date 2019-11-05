854d37fd71dda4f627708df7ba376cc2107d3a98
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-11-05 19:16:56 +0300

    barebone code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again!");
+  }
+}
